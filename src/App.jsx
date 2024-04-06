677db83720e8689a519a3c097f9baa5b03dd17f6
Aryan <lit2021037@iiitl.ac.in>
2024-04-06 15:09:56 +0530

    feat(react): move from react script to vite

---
+++ src/App.jsx
@@ -0,0 +1,139 @@
+import "./App.css";
+
+import React, { useState } from "react";
+import NavBar from "./Components/NavBar";
+import News from "./Components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+const App = () => {
+  const apiKey = import.meta.env.VITE_NEWS_API;
+  const [progress, setProgress] = useState(0);
+
+  const setNewProgress = (progress) => {
+    setProgress(progress);
+  };
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <LoadingBar height={3} color="#f11946" progress={progress} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <News
+                setProgress={setNewProgress}
+                apiKey={apiKey}
+                key="general"
+                pageSize={5}
+                country="in"
+                category="general"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                setProgress={setNewProgress}
+                apiKey={apiKey}
+                key="business"
+                pageSize={5}
+                country="in"
+                category="business"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                setProgress={setNewProgress}
+                apiKey={apiKey}
+                key="entertainment"
+                pageSize={5}
+                country="in"
+                category="entertainment"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/general"
+            element={
+              <News
+                setProgress={setNewProgress}
+                apiKey={apiKey}
+                key="general"
+                pageSize={5}
+                country="in"
+                category="general"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                setProgress={setNewProgress}
+                apiKey={apiKey}
+                key="health"
+                pageSize={5}
+                country="in"
+                category="health"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/sciences"
+            element={
+              <News
+                setProgress={setNewProgress}
+                apiKey={apiKey}
+                key="science"
+                pageSize={5}
+                country="in"
+                category="science"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/ports"
+            element={
+              <News
+                setProgress={setNewProgress}
+                apiKey={apiKey}
+                key="sports"
+                pageSize={5}
+                country="in"
+                category="sports"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                setProgress={setNewProgress}
+                apiKey={apiKey}
+                key="technology"
+                pageSize={5}
+                country="in"
+                category="technology"
+              />
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+export default App;
